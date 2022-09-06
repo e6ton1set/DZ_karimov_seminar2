48050227f3bbdb602353e1858ee1a9d05627ff0d
Ruslan Karimov <ru-ka-@mail.ru>
2022-09-06 17:46:52 +0500

    added example001

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите целое трёхзначное число");
+
+int number = int.Parse(Console.ReadLine());
+
+    if (number > 0)
+{
+string str = number.ToString();
+Console.WriteLine($"Вторая цифра числа {number} это {str[1]}");
+}
+    else
+{
+        Console.WriteLine("Вы ввели отрицательное число");
+}
